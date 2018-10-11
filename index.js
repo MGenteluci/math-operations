7475c17f5eec77b7802e93bd2f162129d48f96d2
MGenteluci <mgenteluci97@gmail.com>
2018-10-11 14:06:36 -0300

    Refactor in spaces

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-exports.fibonacci = n => n === 0 ? 0 : n <= 2 ? 1 : this.fibonacci(n-1) + this.fibonacci(n-2);;
+exports.fibonacci = n => n === 0 ? 0 : n <= 2 ? 1 : this.fibonacci(n-1) + this.fibonacci(n-2);
 
 exports.factorial = n => n === 1 ? 1 : n * this.factorial(n-1);
 
 exports.sum = (...args) => args.reduce((i, total) => total += i);
 
 exports.bubbleSort = arr => {
-  for(let i = 0; i < arr.length; i++){
-    for(let j = 0; j < arr.length-1; j++){
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length-1; j++) {
       if (arr[j] > arr[j+1]) {
         [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
       }
